d7509c7adc74d05a603eea891047fec1dee2d2c6
derzhavin3016 <derzhavin.andrej@yandex.ru>
2019-10-02 21:19:38 +0300

    Unit tests added

--- SquareEq/SqEq.cpp
+++ SquareEq/SqEq.cpp
@@ -5,11 +5,13 @@
 
 #pragma warning (disable: 4996 4018)
 
+#define SQTest(A, B, C, X1, X2, NROOTS) if (!TestSquare(A, B, C, X1, X2, NROOTS)) \
+                                  return 0;
 const double EPSILON = 1e-12;
 
 const int MAXTRIES = 100;
 
-#define MAXCOEFF 3
+const int MAXCOEFF = 3;
 
 __inline int Compare( double a, double b );
 
@@ -22,6 +24,11 @@ int InputNumbers( const char promt[], double* values, size_t amount );
 void FillNan( double* array, size_t size );
 
 void ClearBuffer( void );
+
+int TestSquare(double a, double b, double c, double x1, double x2, int nRoots);
+
+int UnitTests( void );
+
 
 enum roots
 {
@@ -39,6 +46,14 @@ int main( void )
 
   // Filling array with NAN values
   FillNan(coeff, MAXCOEFF);
+
+  if (UnitTests())
+    printf("UnitTests sucess.\n");
+  else
+  {
+    printf("UnitTests failed\n");
+    return 1;
+  }
 
   // Inputing the coefficients
   int OK = InputNumbers("Square equation solver program\n\n"
@@ -152,6 +167,10 @@ int SolveSquare( double a, double b, double c, double *x1, double *x2 )
  */
 int SolveLinear( double a, double b, double *x )
 {
+  assert(isfinite(a));
+	assert(isfinite(b));
+  assert(x != NULL);
+
 	if (Compare(a, 0.0))
 	{
 		if (Compare(b, 0.0))
@@ -200,7 +219,7 @@ int InputNumbers( const char promt[], double *values, size_t amount )
 
     if (is_prev_correct)
     {
-      printf("Input [%d/%d] value: ", correct_input + 1, amount);
+      printf("Input [%d/%zd] value: ", correct_input + 1, amount);
       is_prev_correct = !is_prev_correct;
     }
     incr = scanf("%lg", values + correct_input);
@@ -223,13 +242,13 @@ int InputNumbers( const char promt[], double *values, size_t amount )
  * \param [in]    size   Array size.
  * \return  NONE.
  */
-void FillNan( double* array, size_t size )
+void FillNan( double* arr, size_t size )
 {
-  assert(array != NULL);
+  assert(arr != NULL);
   assert(size != 0);
 
   for (int i = 0; i < size; i++)
-    array[i] = NAN;
+    arr[i] = (double)NAN;
 } /* End of 'FillNan' function */
 
 /**
@@ -246,7 +265,7 @@ double * InputNumbers2( const char promt[], size_t amount )
 
   for (int i = 0; i < MAXTRIES; i++)
   {
-    printf("Input [%d/%d] value: ", correct_input + 1, amount);
+    printf("Input [%d/%zd] value: ", correct_input + 1, amount);
     incr = scanf("%lg", data + correct_input);
     ClearBuffer();
     if (incr)
@@ -267,3 +286,64 @@ void ClearBuffer( void )
   while (sym != '\n')
     sym = getchar();
 } /* End of 'ClearBuffer' function */
+
+/**
+ * \brief Test SolveSquare function (one test).
+ * \param [in]   a    First coefficient.
+ * \param [in]   b    Second coefficient.
+ * \param [in]   c    Third coefficient.
+ * \param [in] *x1    Expected root.
+ * \param [in] *x2    Expected second root.
+ * \return 1 if all is OK
+ * \return 0 otherwise.
+ */
+int TestSquare( double a, double b, double c, double x1, double x2, int nRoots )
+{
+  double Roots[2];
+  if (SolveSquare(a, b, c, Roots, Roots + 1) == nRoots)
+    switch (nRoots)
+    {
+    case SS_ZERO_ROOTS:
+      return 1;
+    case SS_INF_ROOTS:
+      return 1;
+    case SS_ONE_ROOT:
+      if (Roots[0] == x1)
+        return 1;
+      return 0;
+    case SS_TWO_ROOTS:
+      if ((Compare(Roots[0], x1)) && (Compare(Roots[1], x2)) ||
+          (Compare(Roots[0], x2)) && (Compare(Roots[1], x1)))
+        return 1;
+      return 0;
+    default:
+      return 0;
+    }
+  return 1;
+} /* End of 'TestSquare' function */
+
+/**
+ * \brief Unit tests for SolveSquare.
+ * \return 1 if all is OK
+ * \return 0 otherwise.
+ */
+int UnitTests( void )
+{
+  SQTest(1, 1, 1, 0, 0, SS_ZERO_ROOTS);
+
+  SQTest(1, 5, 6, -2, -3, SS_TWO_ROOTS);
+
+  SQTest(1, 0, 0, 0, 0, SS_ONE_ROOT);
+
+  SQTest(0, 5, 6, -6 / 5.0, 0, SS_ONE_ROOT);
+
+  SQTest(0, 0, 6, 0, 0, SS_ZERO_ROOTS);
+
+  SQTest(0, 0, 0, 0, 0, SS_INF_ROOTS);
+
+  SQTest(0, 0.0000001, 5, -5 / 0.0000001, 0, SS_ONE_ROOT);
+
+  SQTest(1, 0, -2, sqrt(2), -sqrt(2), SS_TWO_ROOTS);
+
+  return 1;
+} /* End of 'UnitTests' function */
