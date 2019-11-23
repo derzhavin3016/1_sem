bbf1684383dde3ac39f398f597d06ebdeed5a1d6
derzhavin3016 <derzhavin.andrej@yandex.ru>
2019-11-23 21:18:04 +0300

    Additional files

---
+++ Differentiator/cmd.h
@@ -0,0 +1,43 @@
+#define REC_L rec_calc(nd->left)
+
+#define REC_R rec_calc(nd->right)
+
+
+DEF_OP('+', PLUS, 
+  {
+    return REC_L + REC_R;
+  },
+  {
+    return dL + dR;
+  })
+
+DEF_OP('-', MINUS, 
+  {
+    return REC_L - REC_R;
+  },
+  {
+    return dL - dR;
+  })
+
+DEF_OP('*', MUL, 
+  {
+    return REC_L * REC_R;
+  },
+  {
+    return dL * cR + cL * dR;
+  })
+
+DEF_OP('/', DIV,
+  {
+    double r = REC_R;
+    TREE_ASSERT(r != 0, "Dividing by zero!!");
+    
+    return REC_L / r;
+  },
+  {
+    return (dL * cR - cL * dR) / (cR * cR);
+  })
+
+#undef REC_R
+
+#undef REC_L
