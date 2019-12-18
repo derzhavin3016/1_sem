80d3e2a108ebf486855cbcb34f4800a1387bcbc6
derzhavin3016 <derzhavin.andrej@yandex.ru>
2019-12-18 13:27:37 +0300

    just commands

---
+++ LNG/lang.h
@@ -0,0 +1,16 @@
+
+#define LNG_IF  "fi"
+
+#define LNG_ELSE "esel"
+ 
+#define LNG_WHILE "hewli"
+
+#define LNG_RETURN "rterun"
+ 
+#define LNG_GET  "gte"
+
+#define LNG_PUT  "tup"
+
+#define LNG_MAIN "mani"
+
+#define LNG_VAR "arv"
